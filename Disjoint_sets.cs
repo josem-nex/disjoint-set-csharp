0fc6acae8b134c80e229ee3600a881ed31d153a6
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2023-04-28 22:41:50 -0400

    first_commit

---
+++ Disjoint_sets.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disjoin_sets_Csharp;
+
+public class Disjoint_sets
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Disjoin_sets_Csharp;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
